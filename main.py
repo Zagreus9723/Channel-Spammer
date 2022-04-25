e39d399710d682cb4811ba10f10b00fcb9dd7020
Apophis <61201959+Apophis52@users.noreply.github.com>
2022-04-25 16:53:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,4 @@ bot = discum.Client(token=f'{input("Input token: ")}', log=False)
 message = input("Input message: ")
 channel = input("Input channel ID: ")
 while True:
-  time.sleep(1)
   bot.sendMessage(f"{channel}",f"{message}")
